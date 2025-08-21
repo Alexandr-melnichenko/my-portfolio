274c99adcb5b1fab892d362c54007f0b4d801fc4
Alexandr Melnichenko <alexandr.melnichenko@gmail.com>
2025-08-21 06:17:04 +0300

    fix btn more for skills

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,11 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
   const readMoreSkillsBtn = document.getElementById('moreSkillsBtn');
-  const hiddenSkills = document.querySelectorAll('extended-box');
+  const techList = document.getElementById('techList');
+  const hiddenSkills = techList.querySelectorAll('.extended-box');
 
   readMoreSkillsBtn.addEventListener('click', () => {
-    hiddenSkills.classList.toggle('visible-skills');
+    hiddenSkills.forEach(item => {
+      item.classList.toggle('visible-skills');
+    });
 
-    if (hiddenSkills.classList.contains('visible-skills')) {
+    if (hiddenSkills[0].classList.contains('visible-skills')) {
       readMoreSkillsBtn.textContent = 'HIDE';
     } else {
       readMoreSkillsBtn.textContent = 'MORE';
